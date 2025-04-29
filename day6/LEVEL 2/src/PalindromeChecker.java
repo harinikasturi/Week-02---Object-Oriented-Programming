d1b0fc2f3544c86a65159cb72798be528921872a
harinikasturi <harinikasturi2003@gmail.com>
2025-04-29 12:41:39 +0530

    Added day6

---
+++ day6/LEVEL 2/src/PalindromeChecker.java
@@ -0,0 +1,32 @@
+public class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String cleaned = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public void displayResult() {
+        System.out.println("Text: " + text);
+        System.out.println("Is palindrome? " + isPalindrome());
+    }
+
+    public static void main(String[] args) {
+        PalindromeChecker checker = new PalindromeChecker("Madam");
+        checker.displayResult();
+    }
+}
